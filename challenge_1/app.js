6bf12f6a99ba31dfdfecfebf29cc79f4d7851223
kentacohut <stevenahallam@gmail.com>
2018-02-12 12:31:56 -0800

    Started tic tac toe

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,20 @@
+var board = ['00','01','02','10','11','12','20','21','22'];
 
+var test = ()=>{
+  console.log('Clicked!');
+};
+var x = document.createTextNode('X');
+var append = (id)=>{
+  var node = document.getElementById(id.target.id);
+  node.appendChild(x);
+};
 
+document.getElementById('00').addEventListener('click', append);
+document.getElementById('01').addEventListener('click', append);
+document.getElementById('02').addEventListener('click', append);
+document.getElementById('10').addEventListener('click', append);
+document.getElementById('11').addEventListener('click', append);
+document.getElementById('12').addEventListener('click', append);
+document.getElementById('20').addEventListener('click', append);
+document.getElementById('21').addEventListener('click', append);
+document.getElementById('22').addEventListener('click', append);
