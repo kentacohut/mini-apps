6c29266bf0a9615b34d059162e6db55535b55572
Steve Hallam <StevenAHallam@gmail.com>
2018-02-15 08:53:37 -0800

    Win logic implememented.

--- challenge_3/client/src/App.jsx
+++ challenge_3/client/src/App.jsx
@@ -14,12 +14,13 @@ class App extends React.Component {
     	[0,0,0,0,0,0,0],
     	[0,0,0,0,0,0,0]
     	],
-    	player: true
+    	player: true,
+      winner: null,
+      count: 0
     }
   }
 
   handleClick(column){
-    console.log(column)
   	let board = this.state.board;
   	let player = ()=>{
   		if(this.state.player){
@@ -32,25 +33,60 @@ class App extends React.Component {
         console.log(space)
   			board[i][column] = player();
   			if(player() === 1){
-        		$(space).addClass('red');
+        	$(space).addClass('red');
   			}else{
-  			   $(space).addClass('black');
+  			  $(space).addClass('black');
   			}
-  			this.setState({player : !this.state.player});
+  			
   			this.setState({board: board});
-  			return
+        if(this.checkWin(board)){
+          this.setState({winner: player()});
+          console.log(this.state.winner)
+        }
+        this.setState({count: this.state.count +  1});
+  			this.setState({player : !this.state.player});
+        return
   		}
   	}
   }
 
-  checkWin() {
-
+  checkWin(board) {
+    console.log(board)
+    for (let row = 0; row < board.length; row++) {
+      for (let col = 0; col < board[row].length; col++) {
+        if (col <= 3 && board[row][col] && 
+          board[row][col] === board[row][col+1] &&
+          board[row][col] === board[row][col+2] &&
+          board[row][col] === board[row][col+3]) {
+          return true;
+        }
+        if (row <= 2 && board[row][col] && 
+          board[row][col] === board[row+1][col] &&
+          board[row][col] === board[row+2][col] &&
+          board[row][col] === board[row+3][col]) {
+          return true;
+        }
+        if (row <= 3 && col <= 3 && board[row][col] && 
+          board[row][col] === board[row+1][col+1] &&
+          board[row][col] === board[row+2][col+2] &&
+          board[row][col] === board[row+3][col+3]) {
+          return true;
+        }
+        if (row >= 3 && col <= 3 && board[row][col] && 
+          board[row][col] === board[row-1][col+1] &&
+          board[row][col] === board[row-2][col+2] &&
+          board[row][col] === board[row-3][col+3]) {
+          return true;
+        }
+      }
+    }
   }
 
   render() {
 		return (
 		<div id="board">
           <Board handleClick={this.handleClick.bind(this)} board={this.state.board} player={this.state.player}/>
+          { this.state.winner ? <Results /> : null }
 		</div>
 		)
 	}
